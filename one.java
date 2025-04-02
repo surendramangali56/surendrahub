c2774603d42b5bf16045061ad916ce09bc0e2cf4
unknown <surendramangali56@gmail.com>
2025-04-02 22:22:48 +0530

    Initial commit

---
+++ one.java
@@ -0,0 +1,50 @@
+# generate a code adding two numbers
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
+
+
+# modify the above code by adding extra variable
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        int extraVariable = 0; // Extra variable added
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
+
+public class AddThreeNumbers {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int num3 = 15; // Third number added
+        int sum = num1 + num2 + num3;
+        System.out.println("The sum of " + num1 + ", " + num2 + " and " + num3 + " is: " + sum);
+    }
+}
+
+
+# documant the above code in total text formatting
+first line is the class name and the second line is the main method. 
+The third line is the first number and the fourth line is the second number. 
+The fifth line is the third number. The sixth line is the sum of the three numbers. 
+The seventh line is the output of the program.
+
+public class AddThreeNumbers {
+    public static void main(String[] args) {
+        int num1 = 5; // First number
+        int num2 = 10; // Second number
+        int num3 = 15; // Third number
+        int sum = num1 + num2 + num3; // Sum of the three numbers
+        System.out.println("The sum of " + num1 + ", " + num2 + " and " + num3 + " is: " + sum); // Output of the program
+    }
+}    
